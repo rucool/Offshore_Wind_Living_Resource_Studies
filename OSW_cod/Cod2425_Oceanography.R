1d8fe284e7df89a9ef4f758dc179faa9d177c148
Kaycee Coleman <kaycee.coleman@gmail.com>
2025-04-12 16:01:17 -0400

    Update Cod2425_Oceanography.R

--- OSW_cod/Cod2425_Oceanography.R
+++ OSW_cod/Cod2425_Oceanography.R
@@ -49,16 +49,19 @@ unit1190_sci_feb25 = mutate(unit1190_sci_feb25,
                             sal = ifelse(sal > 100, NaN, sal))
 
 # mission 5: ru34-20250311T1220
-
-
+ru34_sci_mar25 = read_csv("~/Downloads/ru34-20250311T1220-profile-sci-rt_5f2a_6a52_0263.csv", skip=1)
+names(ru34_sci_mar25) = c("date_time","latitude","longitude","depth", 
+                          "cdom","chl","alt","pitch","roll","oxy","pressure",
+                          "sal", "temp", "u", "v", "wdep")
+ru34_sci_mar25$date_time <- as.POSIXct(ru34_sci_mar25$date_time,tz="UTC") # define as UTC
+ru34_sci_mar25 = unique(ru34_sci_mar25) # remove dups
 
 #library(gtools)
 #do.call(smartbind,l)
 whole_mission = rbind(ru34_sci_nov24, unit1190_sci_dec24)
 whole_mission = rbind(whole_mission, ru34_sci_jan25)
 whole_mission = rbind(whole_mission, unit1190_sci_feb25)
-#whole_mission = rbind(whole_mission)
-
+whole_mission = rbind(whole_mission, ru34_sci_mar25)
 
 # plots
 # TEMP
@@ -89,6 +92,12 @@ m3_temp = ggplot() +
 
 m4_temp = ggplot() + 
   geom_point(data = unit1190_sci_feb25, aes(x = date_time, y = -depth, col = temp)) + 
+  scale_colour_cmocean(name = "thermal", limits = c(3.5,18.5))+
+  labs(x = "Date", y = "Depth (m)", col = "Temp. (C)")+ 
+  theme_bw()
+
+m5_temp = ggplot() + 
+  geom_point(data = ru34_sci_mar25 %>% filter(!is.na(temp)), aes(x = date_time, y = -depth, col = temp)) + 
   scale_colour_cmocean(name = "thermal", limits = c(3.5,18.5))+
   labs(x = "Date", y = "Depth (m)", col = "Temp. (C)")+ 
   theme_bw()
@@ -125,6 +134,11 @@ m4_sal = ggplot() +
   labs(x = "Date", y = "Depth (m)", col = "Salinity")+ 
   theme_bw()
 
+m5_sal = ggplot() + 
+  geom_point(data = ru34_sci_mar25 %>% filter(!is.na(sal)), aes(x = date_time, y = -depth, col = sal)) + 
+  scale_colour_cmocean(name = "haline")+
+  labs(x = "Date", y = "Depth (m)", col = "Salinity")+ 
+  theme_bw()
 
 # OXY
 all_oxy = ggplot() + 
@@ -137,6 +151,13 @@ all_oxy = ggplot() +
 
 m1_oxy = ggplot() + 
   geom_point(data = ru34_sci_nov24 %>% filter(!is.na(oxy)), aes(x = date_time, y = -depth, col = oxy)) + 
+  scale_colour_cmocean(name = "deep")+
+  labs(x = "Date", y = "Depth (m)", col = "Oxygen")+ 
+  theme_bw()+ 
+  theme(legend.position="bottom") 
+
+m5_oxy = ggplot() + 
+  geom_point(data = ru34_sci_mar25 %>% filter(!is.na(oxy)), aes(x = date_time, y = -depth, col = oxy)) + 
   scale_colour_cmocean(name = "deep")+
   labs(x = "Date", y = "Depth (m)", col = "Oxygen")+ 
   theme_bw()+ 
@@ -157,27 +178,33 @@ m1_chl = ggplot() +
   labs(x = "Date", y = "Depth (m)", col = "Chl")+ 
   theme_bw()
 
+m5_chl = ggplot() + 
+  geom_point(data = ru34_sci_mar25 %>% filter(!is.na(chl)), aes(x = date_time, y = -depth, col = chl)) + 
+  scale_colour_cmocean(name = "algae")+
+  labs(x = "Date", y = "Depth (m)", col = "Chl")+ 
+  theme_bw()
+
 # combo plots
 ggarrange(all_temp, all_sal, all_chl, all_oxy,
           labels = c("A.", "B.","C.","D."),
           ncol = 1, nrow = 4) %>%
   ggexport(width = 800, height = 800, filename = "cod2425_temp_sal.tiff")
 
-ggarrange(m1_temp, m2_temp, m3_temp, m4_temp,
+ggarrange(m1_temp, m2_temp, m3_temp, m4_temp, m5_temp,
           labels = c("Mission 1", "Mission 2", "Mission 3", "Mission 4"),
           ncol = 1, nrow = 4, common.legend = TRUE, legend="right") %>%#, legend="bottom") %>%
   ggexport(width = 800, height = 800, filename = "cod2425_temp.tiff")
 
-ggarrange(m1_sal, m2_sal, m3_sal, m4_sal,
+ggarrange(m1_sal, m2_sal, m3_sal, m4_sal,  m5_sal,
           labels = c("Mission 1", "Mission 2", "Mission 3", "Mission 4"),
           ncol = 1, nrow = 4, common.legend = TRUE, legend="right") %>%#, legend="bottom") %>%
   ggexport(width = 800, height = 800, filename = "cod2425_sal.tiff")
 
-ggarrange(m1_oxy, m3_oxy,
+ggarrange(m1_oxy, m3_oxy, m5_oxy,
           labels = c("Mission 1", "Mission 3"),
           ncol = 1, nrow = 2)
 
-ggarrange(m1_chl,m3_chl,
+ggarrange(m1_chl, m3_chl, m5_chl,
           labels = c("Mission 1", "Mission 3"),
           ncol = 1, nrow = 2)
 
